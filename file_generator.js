896555a7ace83cac8311b33a1587ebc1256d26b7
Enkel Licenji <111740082+Enkel-Mobile-Developer@users.noreply.github.com>
2022-11-29 06:31:51 -0800

    Update file_generator.js

--- file_generator.js
+++ file_generator.js
@@ -1,14 +1,14 @@
-import fsp from 'fs/promises'
+import fsp from 'fs/promises';
 
 async function find_uniqueValues (input_file){
 
-    const data_file = await fsp.readFile(input_file, 'utf8') 
+    const data_file = await fsp.readFile(input_file, 'utf8');
     const clear_data = data_file.split(/\r?\n/);  
     const unique_values = input_file.filter ((element, i, values) => ar.indexOf(element) === i);
    return result      
 }
 
 
-const display_result = await find_uniqueValues ('./data.csv')
+const display_result = await find_uniqueValues ('./data.csv');
 
 console.log(display_result);
